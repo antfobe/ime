5410c858f7dec9a0ffcb71063e37fd41c1d42b68
r8ktGet <antfob3@gmail.com>
2018-04-11 23:17:25 -0300

    updated loopy to output a csv

--- eps/md3/loopy.sh
+++ eps/md3/loopy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 RED='\033[0;31m';
 GRN='\033[1;32m';
@@ -6,16 +6,17 @@ YEL='\033[1;33m';
 BLU='\033[1;34m';
 NC='\033[0m';
 
-## Outer loop, iterate over 2,2^16,2^24,2^28 array sizes;
-for i in {1,16,24,28} ; do
+## Initialize .csv header
+echo "SIZE_VECTOR, NUM_THREADS, NUM_IFS, AVG5_TIME(s)" >> contention_out.csv;
 
-	## Inner loop, iterate over, 2,32,64,128,256,512,1024 number of threads;
-	for j in {1,5,6,7,8,9,10} ; do
+## Outer loop, iterate over array sizes up to 2^28;
+for j in $(seq 0 28) ; do
+
+	## Inner loop, iterate over number of threads up to 4096 (limit per process is about ~>7000);
+	for k in $(seq 0 12) ; do
 		## Check progress -- in colors !! --
-	 	printf "\t\t${YEL}Execution: ${GRN}$i ${BLU}$j ${NC}\n";
-		## Put some identifiers in output.
-		echo -e "\t\tExecution: Array size:[$((2 ** $i))] Threads:[$((2 ** $j))]\n" >> contention.out;
-		## Run iteration
-		./contention.sh $((2 ** i)) $((2 ** j)) >> contention.out;
+		printf "\t\t${YEL}Execution: ${GRN}$i ${BLU}$j ${NC}\n";
+		## Run iteration, append .csv
+		./contention.sh $((2**$j)) $((2**$k)) >> contention_out.csv;
 	done;
 done;
