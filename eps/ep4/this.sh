32f7775a9bdc133c2338915cf540d87d9e92046c
Fobe <jeanfobe@ime.usp.br>
2018-05-08 01:02:08 -0300

    Will change dir layout tomorrow

--- eps/ep4/this.sh
+++ eps/ep4/this.sh
@@ -2,9 +2,9 @@
 
 echo "Algorithm,Execution_Number,Elapsed_Time_ns,Access_Avg,Access_StdDev,PerThread_Access_Count" > ep4-data.csv;
 
-for i in $(seq 1 8192); do 
-	for j in $(seq 1 64); do
-		./main $i $((2**$j))  >> ep4-data.csv;
+for i in $(seq 0 13); do 
+	for j in $(seq 16 32); do
+		./main $((2**$i)) $((2**$j))  >> ep4-data.csv;
 		
 	done; 
 done; 
