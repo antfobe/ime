d44885464101a55577aee2476ff4d206d9256093
Fobe <jeanfobe@ime.usp.br>
2018-06-13 15:22:21 -0300

    elected nn winner model

--- eps/mac5832_kaggle/4th.py
+++ eps/mac5832_kaggle/4th.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
 
+
+## bring data in
 df = pd.read_csv('train.csv',encoding='latin1', dtype={'SourcePath': str}, );
-print(list(df.columns.values))
 
 le = preprocessing.LabelEncoder()
 for col in df:
@@ -21,7 +22,6 @@ X_train = datanp[:, 0:];
 scaler = preprocessing.StandardScaler()
 scaler.fit(X_train);
 X_train = scaler.transform(X_train);
-print(X_train[0,:]);
 
 testdf = pd.read_csv('test.csv',encoding='latin1', dtype={'SourcePath': str}, );
 
@@ -32,8 +32,69 @@ for col in testdf:
 testnp = testdf.values;
 test = testnp[:, 0:];
 test = scaler.transform(test);
-print(test[0,:]);
 
+## function to plot fits
+def plot_on_dataset(X, y, ax, name):
+    # for each dataset, plot learning for each learning strategy
+    print("\nlearning on dataset %s" % name)
+    ## ax.set_title(name)
+    X = preprocessing.MinMaxScaler().fit_transform(X)
+    mlps = []
+    if name == "digits":
+        # digits is larger but converges fairly quickly
+        max_iter = 15
+    else:
+        max_iter = 400
+
+    best = 0;
+    for label, param in zip(labels, params):
+        print("training: %s" % label)
+        mlp = MLPClassifier(verbose=0, random_state=0,
+                            max_iter=max_iter, **param)
+        mlp.fit(X, y)
+        mlps.append(mlp)
+        print("Training set score: %f" % mlp.score(X, y))
+        print("Training set loss: %f" % mlp.loss_)
+        if mlp.score(X,y) > best:
+            best = mlp.score(X,y);
+            pred = mlp.predict(test);
+
+    ##for mlp, label, args in zip(mlps, labels, plot_args):
+    ##        ax.plot(mlp.loss_curve_, label=label, **args)
+    np.savetxt('nn-mlp-submission.csv', X=np.vstack((testdf.values[:,0], pred)).T, fmt='%d', delimiter=',', header='id,y');
+
+## set multiple params for attempts at best fit
+params = [{'solver': 'sgd', 'learning_rate': 'constant', 'momentum': 0,
+           'learning_rate_init': 0.2},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .9,
+           'nesterovs_momentum': False, 'learning_rate_init': 0.2},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .9,
+           'nesterovs_momentum': True, 'learning_rate_init': 0.2},
+          {'solver': 'sgd', 'learning_rate': 'invscaling', 'momentum': 0,
+           'learning_rate_init': 0.2},
+          {'solver': 'sgd', 'learning_rate': 'invscaling', 'momentum': .9,
+           'nesterovs_momentum': True, 'learning_rate_init': 0.2},
+          {'solver': 'sgd', 'learning_rate': 'invscaling', 'momentum': .9,
+           'nesterovs_momentum': False, 'learning_rate_init': 0.2},
+          {'solver': 'adam', 'learning_rate_init': 0.01},
+          {'solver': 'lbfgs', 'learning_rate': 'constant', 'momentum': .9,
+           'nesterovs_momentum': True, 'learning_rate_init': 0.0001}]
+
+labels = ["constant learning-rate", "constant with momentum",
+          "constant with Nesterov's momentum",
+          "inv-scaling learning-rate", "inv-scaling with momentum",
+          "inv-scaling with Nesterov's momentum", "adam", "lbfgs"]
+
+plot_args = [{'c': 'red', 'linestyle': '-'},
+             {'c': 'yellow', 'linestyle': '-'},
+             {'c': 'blue', 'linestyle': '-'},
+             {'c': 'red', 'linestyle': '--'},
+             {'c': 'green', 'linestyle': '--'},
+             {'c': 'blue', 'linestyle': '--'},
+             {'c': 'black', 'linestyle': '-'},
+             {'c': 'green', 'linestyle': '-'}]
+
+'''
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(12, 8, 5, 3), random_state=1)
 clf.fit(X_train, Y_train)                         
 MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
@@ -44,5 +105,11 @@ MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
        solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
        warm_start=False)
 
-np.savetxt('nn-submission.csv', X=np.vstack((testdf.values[:,0], clf.predict(test))).T, fmt='%d', delimiter=',', header='id,y');
-print([coef.shape for coef in clf.coefs_]);
+pred = clf.predict(test);
+print("Training set score: %f" % clf.score(X_train, Y_train));
+print("Test set score: %f" % clf.score(test, pred));
+'''
+fig, axes = plt.subplots(2, 2, figsize=(15, 10));
+plot_on_dataset(X=X_train, y=Y_train, ax=axes.ravel(), name='kaggle');
+##fig.legend(ax.get_lines(), labels, ncol=3, loc="upper center");
+##plt.show();
