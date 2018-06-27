46ef6c336725acaea7c465879f7f5e3450192c73
antfobe <jeanfobe@ime.usp.br>
2018-06-26 21:17:30 -0300

    belt made of watches

--- eps/mac5832_kaggle/6th.R
+++ eps/mac5832_kaggle/6th.R
@@ -1,11 +1,7 @@
 if(!require("install.load")) {install.packages("install.load"); library("install.load")}
 
 lapply(c('foreach', 'doParallel', 'parallel', 'e1071', 'parallelSVM', 'caret', 'pROC'), 
-       FUN = function(X){
-    if(!X %in% installed.packages()) {
-        install_load(X);
-    }
-});
+       FUN = install_load);
 
 svmseed <- as.numeric(format(Sys.Date(), "%Y"));
 set.seed(svmseed);
@@ -22,31 +18,31 @@ test[, conv2numeric] <- sapply(test[, conv2numeric], FUN = as.numeric);
 
 x <- subset(data, select = c(-y,-id));
 y <- subset(data, select = y);
+xsim <- x[sample(nrow(x), nrow(y)/2),];
 ## woah - cannot use just 't', apparently messes with built-in functions: 
 ## https://stats.stackexchange.com/questions/233531/object-of-type-closure-is-not-subsettable
 test_t <- subset(test, select = c(-id));
 
-svm_model <- parallelSVM::parallelSVM(x, y$y,
+system.time(svm_model <- parallelSVM::parallelSVM(xsim, y$y,
                                       type = "C-classification",
                                       kernel = "radial",
                                       seed = svmseed,
                                       probability = TRUE, 
                                       gamma = 0.004487103, cost = 3,
-                                      numberCores = parallel::detectCores()-1);
-summary(svm_model);
+                                      numberCores = parallel::detectCores()-1));
 
 ## performance
 system.time(pred <- predict(svm_model, x));
 pred_numeric <- as.double(as.character(pred));
 pred_numeric[pred_numeric < 0] <- 0.0;
-get_perf <- function(a,b) {return (min(a,b)/max(a,b));}
 cat("Performance : [", 
-    (get_perf(length(round(pred_numeric)[round(pred_numeric) == 1]), length(y$y[y$y == 1])) + 
-    get_perf(length(round(pred_numeric)[round(pred_numeric) == 0]), length(y$y[y$y == 0])))/2, "]\n");
+    length(pred[pred == y$y])/nrow(y), 
+    "], #y = ", length(round(pred_numeric)[round(pred_numeric) == 1]),"\n");
 
 system.time(pred <- predict(svm_model, test_t));
 pred_numeric <- as.double(as.character(pred));
 pred_numeric[pred_numeric < 0] <- 0.0;
+cat("#y = ", length(round(pred_numeric)[round(pred_numeric) == 1]),"\n");
 
 write.csv(data.frame(id = test$id, pred = pred_numeric), file = "parallel-nontuned.csv", row.names = FALSE);
 write.csv(data.frame(id = test$id, pred = round(pred_numeric)), file = "parallel-nontuned-rounded.csv", row.names = FALSE);
@@ -54,13 +50,10 @@ write.csv(data.frame(id = test$id, pred = round(pred_numeric)), file = "parallel
 ## Tunning
 ### SPLIT DATA INTO K FOLDS ###
 ## sets seed as 4 digits current year
-set.seed(
-    as.numeric(
-        format(Sys.Date(), "%Y")));
 
-x$fold <- caret::createFolds(1:nrow(x), k = 4, list = FALSE);
+x$fold <- caret::createFolds(1:nrow(x), k = 10, list = FALSE);
 ### PARAMETER LIST ###
-parms <- expand.grid(cost = c(10, 100), gamma = c(1, 2));
+parms <- expand.grid(cost = c(2.4, 2.5), gamma = c(0.004518313, 0.0044));
 ### LOOP THROUGH PARAMETER VALUES ###
 result <- foreach::foreach(i = 1:nrow(parms), .combine = rbind) %do% {
     c <- parms[i, ]$cost;
--- eps/mac5832_kaggle/7th.py
+++ eps/mac5832_kaggle/7th.py
@@ -1,36 +1,43 @@
-import numpy
-import pandas
-import math
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Activation,Dense,Flatten,Convolution1D,Lambda
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
-from keras import optimizers
+from keras import backend,optimizers
 from sklearn.model_selection import cross_val_score
-from sklearn import preprocessing
 from sklearn.model_selection import StratifiedKFold
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 seed = 13
-numpy.random.seed(seed)
+np.random.seed(seed)
 
-df = pandas.read_csv('train.csv',encoding='latin1', dtype={'SourcePath': str}, );
+
+# load/treat data
+df = pd.read_csv('train.csv',encoding='latin1', dtype={'SourcePath': str}, );
+testdf = pd.read_csv('test.csv',encoding='latin1', dtype={'SourcePath': str}, );
 
 le = preprocessing.LabelEncoder()
 for col in df:
 	if col in ["age", "job", "marital", "default", "housing", "loan", "contact", "poutcome", "education"]: 
 		df[col] = le.fit_transform(df[col]);
+		if col in testdf:
+			testdf[col] = le.fit_transform(testdf[col]);
 
 df = df.drop(['id'], axis = 1);
+testdf = testdf.drop(['id'], axis = 1);
 
 datanp = df.values;
 Y = datanp[:, 0].astype(int);
-#Y = le.fit_transform(Y);
 datanp = df.drop(['y'], axis = 1).values;
 X = datanp[:, 0:].astype(float);
 dummy_y = np_utils.to_categorical(Y, num_classes=2);
+T = testdf.values[:,0:].astype(float);
 
 def create_baseline():
 	# create model
@@ -40,14 +47,13 @@ def create_baseline():
 	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 	# Compile model
 	adad = optimizers.Adadelta(lr=1.1, rho=0.95, epsilon=None, decay=0.0)
+	sgd = optimizers.SGD(lr=.0011, momentum=0.0, decay=0.0, nesterov=True)
 	model.compile(loss='binary_crossentropy', optimizer=adad, metrics=['accuracy'])
-	#sgd = optimizers.SGD(lr=0.8, decay=1e-6, momentum=0.9, nesterov=True)
 	#model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 	#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	#model.fit(X, Y, epochs=100, batch_size=df.shape[0])
 	return model
 
-print(df.shape[0])
 # evaluate model with standardized dataset
 estimators = []
 estimators.append(('standardize', StandardScaler()))
@@ -56,4 +62,16 @@ pipeline = Pipeline(estimators)
 kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=seed)
 results = cross_val_score(pipeline, X, Y, cv=kfold)
 print("Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
-
+'''
+# predict on test set
+pipeline.fit(X, Y)
+pred = pipeline.predict(T)
+print(pred.shape, "-", np.arange(1, pred.size + 1, dtype=int).reshape(pred.size,1).shape)
+#np.savetxt('nn-keras-submission.csv', X=np.dstack((np.arange(1, pred.size + 1), pred)), fmt='%d,%d', delimiter=',', header='id,y', comments='');
+with open('nn-keras-submission.csv', 'w') as f:
+	f.write('\"id\",\"y\"\n')
+	itr = 0
+	for value in np.nditer(pred):
+		f.write(str(itr) + ',' + str(value) + '\n')
+		itr = itr + 1
+'''
